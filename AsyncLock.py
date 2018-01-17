924c06dbe6aa10541d02528f2733c12ab45a7fca
Too-Naive <sometimes.naive@hotmail.com>
2018-01-17 21:39:38 +0800

    Add asynclock.py * Change copyright year

---
+++ AsyncLock.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# AsyncLock.py
+# Copyright (C) 2017-2018 Too-Naive and contributors
+#
+# This module is part of libpy and is released under
+# the GPL v3 License: https://www.gnu.org/licenses/gpl-3.0.txt
+import time
+from threading import Thread
+
+class AsyncLock:
+	def __init__(self,wait_time,sleep_time=0.01):
+		self.wait_time = wait_time
+		self.sleep_time = sleep_time
+		self.lock = False
+	def setLock(self):
+		if self.lock:
+			self.waitForUnlock()
+		self.lock = True
+		t = Thread(target=self.sleepProcess)
+		t.daemon = True
+		t.start()
+	def sleepProcess(self):
+		time.sleep(self.wait_time)
+		self.lock = False
+	def waitForUnlock(self):
+		while self.lock:
+			time.sleep(self.sleep_time)
+
